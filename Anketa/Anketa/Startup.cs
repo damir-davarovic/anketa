e4510344ddc4c338eb1d6c03453bd2c60ad816f9
Damir Davarović <damir.davarovic@gmail.com>
2015-08-30 16:44:58 +0200

    Installed online template.

---
+++ Anketa/Anketa/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Anketa.Startup))]
+namespace Anketa
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
