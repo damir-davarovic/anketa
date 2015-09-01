64d33487f7300d5eadc2ce73b3382be2e747d94b
Damir Davarović <damir.davarovic@gmail.com>
2015-09-01 21:47:53 +0200

    Added Question.cs

---
+++ Anketa/Anketa/Models/Question.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Anketa.Models
+{
+    public enum TipPitanja
+    {
+        single, multiple
+    }
+
+    public class Question
+    {
+        public int ID { get; set; }
+        public int surveyID { get; set; }
+        public TipPitanja? TipPitanja { get; set; }
+        public bool aktivnoPitanje { get; set; }
+    }
+}
