543cd35befa913e4edc97a8b00c1a9bc7a26bad0
Damir Davarović <damir.davarovic@gmail.com>
2015-09-01 23:23:21 +0200

    Created SurveyContext for connection to database.

---
+++ Anketa/Anketa/DAL/SurveyContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using Survey = Anketa.Models.Survey;
+using Question = Anketa.Models.Question;
+using Answer = Anketa.Models.Answer;
+
+namespace Anketa.DAL
+{
+    public class SurveyContext : DbContext
+    {
+        public SurveyContext() : base("SurveyContext")
+        {
+        }
+
+        public DbSet<Survey> Surveys { get; set; }
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<Answer> Answers { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
