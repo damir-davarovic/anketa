1fee282fbbd0cea2dcafbfbd4b2238cbef907320
Cubelaster <david.cubela@gmail.com>
2015-09-01 21:46:40 +0200

    Added Survey.cs

---
+++ Anketa/Anketa/Models/Survey.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Anketa.Models
+{
+    public class Survey
+    {
+        public int surveyID { get; set; }
+        public int ownerId { get; set; }
+        public String surveyName { get; set; }
+        public DateTime creationDate { get; set; }
+        public bool surveyActive { get; set; }
+
+        public virtual ICollection<Question> Question { get; set; };
+    }
+}
