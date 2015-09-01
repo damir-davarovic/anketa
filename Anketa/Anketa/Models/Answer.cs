d13d17d20b3993258f11f6b9a30d07d6280e1f49
Damir Davarović <damir.davarovic@gmail.com>
2015-09-01 22:31:28 +0200

    Created the answer class.

---
+++ Anketa/Anketa/Models/Answer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Anketa.Models
+{
+    public class Answer
+    {
+        public int ID { get; set; }
+        public int questionID { get; set; }
+        public bool tocno { get; set; }
+    }
+}
