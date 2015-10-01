bb57407b0d9f91c384c378aa47c860c32888960f
Damir Davarović <damir.davarovic@gmail.com>
2015-10-02 00:41:03 +0200

    Frontend for survey creation.

--- Anketa/Anketa/Scripts/scripts.js
+++ Anketa/Anketa/Scripts/scripts.js
@@ -2,4 +2,27 @@
     $('#all-surveys').DataTable();
     $('#my-surveys').DataTable();
     $(".disabled").prop("disabled", true);
+
+    $(".enter-survey-questions .dropdown-menu li a").click(function () {
+        $(this).parents(".dropdown").find('.btn').html($(this).text() + ' <span class="caret"></span>');
+        $(this).parents(".dropdown").find('.btn').val($(this).data('value'));
+    });
+
+
+    $(document).on("click", ".duplicate-input-group", function () {
+        if($(this).parent(".panel-footer").prev(".panel-body").find(".answer").is(":hidden")){
+            $(this).parent(".panel-footer").prev(".panel-body").find(".answer:hidden").show();
+        }else{
+            $(this).parent(".panel-footer").prev(".panel-body").find(".answer:first").clone().appendTo($(this).parent(".panel-footer").prev(".panel-body")).show();
+        }
+    });
+
+    $(document).on("click", ".delete-answer", function () {
+        console.log($(this).parents(".panel-body").find(".answer").length);
+        if ($(this).parents(".panel-body").find(".answer").length == 1) {
+            $(this).parents(".answer").hide();
+        } else {
+            $(this).parents(".answer").remove();
+        }
+    });
 });
